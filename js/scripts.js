b758c3721d4d7c0d0ace6224fa199161467a0eac
aostrovska <anastasija.ostrovska25@gmail.com>
2021-04-24 11:55:05 +0300

    First ommit

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 async function request(url) {
 	var body = {
-			"login" : document.getElementById("login").value,
-			"pass" : document.getElementById("password").value
+			"username" : document.getElementById("username").value,
+			"password" : document.getElementById("password").value
 		}
 		let res = await fetch(url, {
 		method: 'POST', // *GET, POST, PUT, DELETE, etc.
@@ -24,11 +24,32 @@ async function request(url) {
 
 	
 }
+async function  request2(url) {
+		let res = await fetch(url, {
+		method: 'GET', // *GET, POST, PUT, DELETE, etc.
+		mode: 'cors', // no-cors, *cors, same-origin
+		cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+		credentials: 'same-origin', // include, *same-origin, omit
+		headers: {
+		  'Content-Type': 'application/json'
+		  // 'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		redirect: 'follow', // manual, *follow, error
+		referrerPolicy: 'no-referrer', // no-referrer, *client
+	  });
+	  let inf = await res;
+		console.log(inf);
+	
+	let jso = await res.text();
+		document.getElementById("resp").innerHTML = jso;
+
+	
+}
 
 function log() {
-		request("http://localhost:8080/");
+		request("http://localhost:8080/login");
 }
 
 function dat() {
-	//	request(document.getElementById("url").value, document.getElementById("body").value);
+		request2("http://localhost:8080/data");
 }
--- serever.go
+++ serever.go
@@ -15,41 +15,47 @@ type logPas struct{
 	Password string `password`
 }
 
-var user = logPas{
-  Username: "username",
-  Password: "password",
-}
+var user = logPas{"username", "password"}
 var token string
 
 func setupResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
     (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
 func Handler(w http.ResponseWriter, req *http.Request) {
 	setupResponse(&w, req)
-	data, err := ioutil.ReadAll(req.Body)
-	req.Body.Close()
-	var person logPas
-	err = json.Unmarshal(data, &person)
-	if err != nil{
-		return 
+	if (*req).Method == "OPTIONS" {
+		w.WriteHeader(204)
+	}else if req.Method == "POST" {
+		data, err := ioutil.ReadAll(req.Body)
+		req.Body.Close()
+		var person logPas
+		err = json.Unmarshal(data, &person)
+		if err != nil{
+			return 
+		}
+		fmt.Println("handler1", person)
+		if (person.Username == user.Username)&&(person.Password == user.Password){
+			token = "12345"
+		}else {
+			token = " "
+			io.WriteString(w, "incorrect login")
+		}
 	}
-	fmt.Println("handler1")
-	if (person.Username == user.Username)&&(person.Password == user.Password){
-		token = "12345"
-	}else {
-		io.WriteString(w, "incorrect login")
-	}
-	
 }
 
 func Handler2(w http.ResponseWriter, req *http.Request) {
-	if (token == "12345"){
-		io.WriteString(w, "you succcessfuly gained data")
-	}else{
-		io.WriteString(w, "you unsucccessfuly gained data")
+	setupResponse(&w, req)
+	if (*req).Method == "OPTIONS" {
+		w.WriteHeader(204)
+	}else if req.Method == "GET" {
+		if (token == "12345"){
+			io.WriteString(w, "you succcessfuly gained data")
+		}else{
+			io.WriteString(w, "401")
+		}
 	}
 
 
